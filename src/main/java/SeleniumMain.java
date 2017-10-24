519960f85913b50f3c7c2b777153bc42380d7e01
Raul nava <rulas12@gmail.com>
2017-10-24 15:02:48 -0500

    Initial Commit

---
+++ src/main/java/SeleniumMain.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by kmpw648 on 24/10/2017.
+ */
+public class SeleniumMain {
+
+    public static void main(String[] args){
+        WebDriver driver = new FirefoxDriver();
+
+    }
+
+}
